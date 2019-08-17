db0f71c391d2a7628c1ed1f2f3d10f3e45c3d7b4
Hieu Pham <vanhieu1601@gmail.com>
2019-08-17 23:08:47 +0700

    log out error

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -7,15 +7,21 @@ import { UserService } from "src/app/services/user.service";
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
-  menus: [{ name: "Profile"; do: 1 }];
+  choices: any;
 
-  constructor(public user: UserService) {}
+  constructor(public user: UserService) {
+    this.choices = [
+      { name: "Profile", do: this.toProFile, matIcon: "folder_shared" },
+      { name: "Theme", do: this.changeTheme, matIcon: "invert_colors" },
+      { name: "Sign out", do: this.user.signOut, matIcon: "keyboard_return" }
+    ];
+  }
 
   ngOnInit() {}
 
-  isGuest() {
-    return true;
+  toProFile() {
+    console.log(this.user);
   }
 
-  toProFile = () => {};
+  changeTheme() {}
 }
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,24 +1,42 @@
 import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/auth";
 import { auth } from "firebase";
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: "root"
 })
 export class UserService {
-  user: any;
-  constructor(private auth: AngularFireAuth) {
-    this.auth.user.subscribe(user => {
+  private _user: any;
+  logged = false;
+
+  get name() {
+    return this._user.displayName;
+  }
+  get email() {
+    return this._user.email;
+  }
+  get avatar() {
+    return this._user.photoURL;
+  }
+
+  constructor(private _afAuth: AngularFireAuth, private _router: Router) {
+    this._afAuth.user.subscribe(user => {
       if (user != null) {
-      }
+        this._user = user;
+        this.logged = true;
+      } else this.logged = false;
     });
   }
 
   async loginGoogle() {
-    const provider = new auth.GoogleAuthProvider();
-    await this.auth.auth.signInWithPopup(provider);
-    this.user = this.auth.auth.currentUser;
+    await this._afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    this._user = this._afAuth.auth.currentUser;
   }
 
-  getUserInfo() {}
+  signOut() {
+    this._afAuth.auth.signOut().then(() => {
+      this._router.navigate(["/"]);
+    });
+  }
 }
