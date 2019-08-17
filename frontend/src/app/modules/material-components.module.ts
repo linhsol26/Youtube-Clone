7bebfc0d425e6cb5e37c2066655b40eb58ed58fd
Hieu Pham <vanhieu1601@gmail.com>
2019-08-17 11:45:40 +0700

    main menu added

--- frontend/src/app/modules/material-components.module.ts
+++ frontend/src/app/modules/material-components.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
-import { MatMenuModule } from "@angular/material/menu";
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
     BrowserAnimationsModule,
     MatIconModule,
     MatButtonModule,
-    MatMenuModule
+    MatToolbarModule
   ]
 })
 export class MaterialComponentsModule {}
