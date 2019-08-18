8168cb894a8e073d75ffbd59abee875ce8202863
Hieu Pham <vanhieu1601@gmail.com>
2019-08-18 10:18:45 +0700

    fixed sign out error

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -7,21 +7,20 @@ import { UserService } from "src/app/services/user.service";
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
-  choices: any;
+  choices = [
+    { name: "Profile", do: this.toProfile, matIcon: "folder_shared" },
+    { name: "Theme", do: this.changeTheme, matIcon: "invert_colors" },
+    {
+      name: "Sign out",
+      do: () => this.user.signOut(),
+      matIcon: "keyboard_return"
+    }
+  ];
 
-  constructor(public user: UserService) {
-    this.choices = [
-      { name: "Profile", do: this.toProFile, matIcon: "folder_shared" },
-      { name: "Theme", do: this.changeTheme, matIcon: "invert_colors" },
-      { name: "Sign out", do: this.user.signOut, matIcon: "keyboard_return" }
-    ];
-  }
-
+  constructor(private user: UserService) {}
   ngOnInit() {}
 
-  toProFile() {
-    console.log(this.user);
-  }
+  toProfile() {}
 
   changeTheme() {}
 }
