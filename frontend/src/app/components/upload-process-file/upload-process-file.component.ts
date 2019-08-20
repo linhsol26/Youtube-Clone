02a442788edc9cedc75a67ada7e32f34ece94110
Hieu Pham <vanhieu1601@gmail.com>
2019-08-20 09:00:10 +0700

    upload process file component

--- frontend/src/app/components/upload-process-file/upload-process-file.component.ts
+++ frontend/src/app/components/upload-process-file/upload-process-file.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class UploadProcessFileComponent implements OnInit {
   @Input() file: any;
-  uploaded = true;
+  uploaded = false;
   paused = false;
   progress: number = 0;
 
