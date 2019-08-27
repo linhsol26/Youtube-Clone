bbfdcfc658fbf0a934bc4844e51e0b795e7f5d53
Hieu Pham <vanhieu1601@gmail.com>
2019-08-27 15:01:08 +0700

    home page added

--- frontend/src/app/pipes/period.pipe.ts
+++ frontend/src/app/pipes/period.pipe.ts
@@ -5,7 +5,28 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class PeriodPipe implements PipeTransform {
   transform(timestamp: number): string {
-    
-    return null;
+    let diff = Math.round((Date.now() - timestamp) / 1000);
+    let unit = "";
+    if (diff < 60) {
+      // minute
+      unit = diff == 1 ? " second" : " seconds";
+    } else if (diff < 3600) {
+      // hour
+      diff = Math.round(diff / 60);
+      unit = diff == 1 ? " minute" : " minutes";
+    } else if (diff < 86400) {
+      // day
+      diff = Math.round(diff / 3600);
+      unit = diff == 1 ? " hour" : " hours";
+    } else if (diff < 2592000) {
+      // month
+      diff = Math.round(diff / 86400);
+      unit = diff == 1 ? " month" : " months";
+    } else if (diff < 31104000) {
+      // year
+      diff = Math.round(diff / 259200);
+      unit = diff == 1 ? " year" : " years";
+    }
+    return diff + unit + " ago";
   }
 }
