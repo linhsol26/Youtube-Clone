275454cc3fa0132ded26ba49fbf8ef564bf02e5c
linhsol26 <nhglinh.sol@gmail.com>
2020-07-27 09:04:58 +0700

    done check

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,13 +5,20 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyADQMecFrLeUMQqGxPLhB_BWyowO_BDcg0",
-    authDomain: "fir-demo-5413c.firebaseapp.com",
-    databaseURL: "https://fir-demo-5413c.firebaseio.com",
-    projectId: "fir-demo-5413c",
-    storageBucket: "fir-demo-5413c.appspot.com",
-    messagingSenderId: "676506254038",
-    appId: "1:676506254038:web:a705e3db918b6ef1"
+    // apiKey: "AIzaSyADQMecFrLeUMQqGxPLhB_BWyowO_BDcg0",
+    // authDomain: "fir-demo-5413c.firebaseapp.com",
+    // databaseURL: "https://fir-demo-5413c.firebaseio.com",
+    // projectId: "fir-demo-5413c",
+    // storageBucket: "fir-demo-5413c.appspot.com",
+    // messagingSenderId: "676506254038",
+    // appId: "1:676506254038:web:a705e3db918b6ef1"
+    apiKey: 'AIzaSyBbDrbdlADT4ybcVgoZUTwx7-XeYNj8s7I',
+    authDomain: 'utu-be.firebaseapp.com',
+    databaseURL: 'https://utu-be.firebaseio.com',
+    projectId: 'utu-be',
+    storageBucket: 'utu-be.appspot.com',
+    messagingSenderId: '346749165019',
+    appId: '1:346749165019:web:a10400393eeeb6844f1bfe'
   }
 };
 
